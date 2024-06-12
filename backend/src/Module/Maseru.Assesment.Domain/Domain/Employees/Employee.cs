94d361b2a46222a29841b2f43b4031062af285b9
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2024-06-13 00:16:25 +0200

    added Employee, Skill entity classes and SkillLevel enum

---
+++ backend/src/Module/Maseru.Assesment.Domain/Domain/Employees/Employee.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+
+namespace Maseru.Assesment.Domain.Employees
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Employee : FullAuditedEntity<Guid>
+    {
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual DateTime? DateOfBirth { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string PhoneNumber { get; set; }
+
+        #region Address information
+        public virtual string Address { get; set; }
+        public virtual string City { get; set; }
+        public virtual long PostalCode { get; set; }
+        public virtual string Country { get; set; }
+        #endregion
+    }
+}
---
+++ backend/src/Module/Maseru.Assesment.Domain/Domain/Enums/RefListSkillLevel.cs
@@ -0,0 +1,17 @@
+﻿using Shesha.Domain.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maseru.Assesment.Domain.Enums
+{
+	[ReferenceList("Assesment", "SkillLevel")]
+	public enum RefListSkillLevel: long
+	{
+		Beginner = 1,
+		Intermediate = 2,
+		Expert = 3
+	}
+}
