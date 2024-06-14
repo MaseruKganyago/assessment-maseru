e2a2e3691b2b6fd41c19064dbe71edc7cefae4c2
Maseru Kganyago <maseru.kganyago@boxfusion.io>
2024-06-14 11:53:54 +0200

    fixed create/update issues in EmployeeManager

---
+++ backend/src/Module/Maseru.Assesment.Application/Employees/Dtos/SkillDto.cs
@@ -0,0 +1,24 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Maseru.Assesment.Domain.Employees;
+using Maseru.Assesment.Domain.Enums;
+using Maseru.Assesment.Domain.Skills;
+using Shesha.AutoMapper.Dto;
+using Shesha.Services.ReferenceLists.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maseru.Assesment.Employees.Dtos
+{
+    [AutoMap(typeof(Skill))]
+    public class SkillDto : EntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public long? YearsOfExperience { get; set; }
+        public ReferenceListItemValueDto SkillLevel { get; set; }
+        public EntityReferenceDto<Guid?> Employee { get; set; }
+    }
+}
